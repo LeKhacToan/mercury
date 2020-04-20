1dde43013af9446724d5f6de4cc7dbd9791fdebe
Le Khac Toan <lkhactoan@gmail.com>
2020-04-20 23:13:35 +0700

    feat(*): lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    semi: [1, 'never'],
+    curly: ['error', 'multi-line'],
+  },
 };
