84dc93fe7642f68f4e0b68423b3325ed238486d1
Le Khac Toan <lkhactoan@gmail.com>
2020-04-22 23:11:19 +0700

    feat(*): set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { StatusBar, Text } from 'react-native'
+import { Text } from 'react-native'
 
 const App: React.FC = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <Text>hello the gio</Text>
     </>
   )
