fd9b139c9cbde1aa1d882484387e457f61354c52
Le Khac Toan <lkhactoan@gmail.com>
2020-05-05 20:44:55 +0700

    feat(*): update ver

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -3,13 +3,13 @@ import { Provider as PaperProvider } from 'react-native-paper'
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 
-import App from './App'
+import AppNavigator from './AppNavigator'
 
 const AppProvider: React.FC = () => {
   return (
     <NavigationContainer>
       <PaperProvider>
-        <App />
+        <AppNavigator />
       </PaperProvider>
     </NavigationContainer>
   )
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {} from 'react-native'
+
+import { TaskCard } from 'components'
+
+const Home: React.FC = () => {
+  return <TaskCard />
+}
+
+export default Home
